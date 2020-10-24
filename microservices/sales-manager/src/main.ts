eb826c33f9882bad33a74462d08069717ee2058e
Javier Escandarani <javoescan@gmail.com>
2020-10-24 17:44:40 -0300

    docker-compose

--- microservices/sales-manager/src/main.ts
+++ microservices/sales-manager/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const SERVER_PORT = process.env.SERVER_PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(SERVER_PORT);
 }
 bootstrap();
